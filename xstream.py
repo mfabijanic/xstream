d2315d8769961c8ff004898636b93a39a4c70d91
Matej Fabijanic <root4unix@gmail.com>
2023-07-09 09:22:21 +0200

    Add Gtk and GstVideo version

--- xstream.py
+++ xstream.py
@@ -32,7 +32,9 @@ debug = 0
 
 
 import gi
+gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
+gi.require_version('GstVideo', '1.0')
 from gi.repository import GObject, Gtk, GdkPixbuf, Gdk
 
 # Needed for window.get_xid(), xvimagesink.set_window_handle(), respectively:
